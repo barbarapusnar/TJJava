8c48c07a7ca029c19cc50aa13376337ea6711ac7
Barbara Pušnar <barbara.pusnar@scng.si>
2021-10-14 17:55:21 +0200

    Začetni projekt java

---
+++ IgraKrizciKrozci/src/krizcikrozci/KrizciKrozci.java
@@ -0,0 +1,17 @@
+
+package krizcikrozci;
+import javax.swing.*;
+
+public class KrizciKrozci {
+    public static void main(String[] args)
+    {
+    SwingUtilities.invokeLater(new Runnable()
+    {
+        //stran 266 in 267
+    public void run() {
+    JPanel glavnaPlošča=new GlavnaPlosca();
+    GraficnoOgrodje.ustvariOkno("Križci in krožci", glavnaPlošča, true);
+    }
+    });
+    }
+}
